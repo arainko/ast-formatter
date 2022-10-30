cc0e076fa33e1c932655cbb5698a19def7fddb12
arainko <aleksander.rainko99@gmail.com>
2022-10-30 15:32:41 +0100

    use cats effect for io and printing

--- Main.scala
+++ Main.scala
@@ -1,25 +1,32 @@
-import fansi.*
 import com.monovore.decline.*
+import com.monovore.decline.effect.CommandIOApp
 import cats.syntax.all.*
-
-import java.nio.file.*
+import fs2.io.file.*
+import cats.effect.*
+import cats.effect.std.Console
+import cats.parse.*
+import scala.util.control.NoStackTrace
 
 object Main
-    extends CommandApp(
+    extends CommandIOApp(
       name = "ast-formatter",
-      header = "Formats the AST printed with Printer.TreeStructure",
-      main = {
-        val fileOption =
-          Opts
-            .option[String]("filepath", "the path to the structure file", "f", "path")
-            .map(Path.of(_))
+      header = "Formats the AST printed with Printer.TreeStructure"
+    ) {
 
-        fileOption.map(Files.readString).map { contents =>
-          AST
-            .parse(contents)
-            .map(Printer.print)
-            .map(println)
-            .leftMap(err => println(s"Encountered error during parsing: $err"))
-        }
-      }
-    )
+  override def main: Opts[IO[ExitCode]] =
+    Opts
+      .option[String]("filepath", "the path to the structure file", "f", "path")
+      .map(Path.apply)
+      .map(processFile)
+
+  private def processFile(path: Path) =
+    for {
+      contents <- Files[IO].readUtf8Lines(path).compile.string
+      ast <- IO.fromEither(AST.parse(contents).left.map(ParseException(_)))
+      _ <- Console[IO].println(Printer.print(ast))
+    } yield ExitCode.Success
+
+  private final class ParseException(error: Parser.Error) extends NoStackTrace {
+    override def getMessage(): String = s"Encountered error during parsing: ${error.toString()}"
+  }
+}
--- conf.scala
+++ conf.scala
@@ -1,4 +1,5 @@
 //> using scala "3.2.0"
 //> using lib "org.typelevel::cats-parse:0.3.7"
 //> using lib "com.lihaoyi::fansi:0.4.0"
-//> using lib "com.monovore::decline:2.3.1"
+//> using lib "com.monovore::decline-effect:2.3.1"
+//> using lib "co.fs2::fs2-io:3.3.0"
