839a8e85524c5c4f3fa92dfc87b613718d84a4a4
Aleksander Rainko <aleksander.rainko99@gmail.com>
2022-11-05 22:36:53 +0100

    add hue

--- Colorful.scala
+++ Colorful.scala
@@ -2,7 +2,42 @@ import fansi.*
 
 object Colorful {
   def apply(text: String, color: Attr)(using useColor: UseColor): String =
-    useColor match
+    useColor match {
       case UseColor.Yes => Str(text).overlay(color).render
       case UseColor.No  => text
+    }
+
+  val Hue = 
+    Vector(
+      Color.True(204, 51, 51),
+
+      Color.True(204, 89, 51),
+      Color.True(204, 128, 51),
+      Color.True(204, 166, 51),  
+      Color.True(204, 204, 51),
+
+      Color.True(166, 204, 51),
+      Color.True(128, 204, 51),
+      Color.True(89, 204, 51),
+      Color.True(51, 204, 51),
+
+      Color.True(51, 204, 89),
+      Color.True(51, 204, 128),
+      Color.True(51, 204, 166),
+      Color.True(51, 204, 204),
+
+      Color.True(51, 166, 204),
+      Color.True(51, 128, 204),
+      Color.True(51, 89, 204),
+      Color.True(51, 51, 204),
+
+      Color.True(89, 51, 204),
+      Color.True(128, 51, 204),
+      Color.True(166, 51, 204),
+      Color.True(204, 51, 204),
+
+      Color.True(204, 51, 166),
+      Color.True(204, 51, 128),
+      Color.True(204, 51, 89),
+    )
 }
